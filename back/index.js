4e840f414ae83acd1ca2dcc67da10459b3602252
Jessy Pinchon <jessy.pinchon@gmail.com>
2020-01-18 23:09:40 +0100

    GET Routes created

--- back/index.js
+++ back/index.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const revenues_2018_per_agency_monthly = require('./figures/revenues_2018_per_agency_monthly.json')
+const revenues_2018_per_agency = require('./figures/revenues_2018_per_agency.json')
+const revenues_2018_per_month = require('./figures/revenues_2018_per_month.json')
+
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to Express');
+});
+
+app.get('/figures/per-agency-monthly', (req, res) => {
+  res.send(revenues_2018_per_agency_monthly)
+});
+
+app.get('/figures/per-agency', (req, res) => {
+  res.send(revenues_2018_per_agency)
+});
+
+app.get('/figures/per_month', (req, res) => {
+  res.send(revenues_2018_per_month)
 });
 
 app.listen(port, err => {
