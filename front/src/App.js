59184acf0f0a8c58dedf6720c0852ab17c6e4467
Jessy Pinchon <jessy.pinchon@gmail.com>
2020-01-19 02:05:42 +0100

    axios installed + method axios.get

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CallTestJson from './components/CallTestJson';
 
 function App() {
   return (
     <div className="App">
-
+      <CallTestJson />
     </div>
   );
 }
