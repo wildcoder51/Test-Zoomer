a0388d4c771532e5113088469c06571ba0f0aea4
Jessy Pinchon <jessy.pinchon@gmail.com>
2020-01-28 23:35:12 +0100

    style for monthglobal chart

--- front/src/components/ByMonthGlobal/ByMonthGlobal.jsx
+++ front/src/components/ByMonthGlobal/ByMonthGlobal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import {Bar} from 'react-chartjs-2';
+import moment from 'moment'
 
 
 class ByMonthGlobal extends React.Component {
@@ -26,14 +27,30 @@ class ByMonthGlobal extends React.Component {
 
   getChartDatas= () => {
     return({
-      labels : this.state.figurePerMonth.map(el => el.time),
+      labels : this.state.figurePerMonth.map(el => moment(el.time).format('MMMM')),
       datasets : [{
         label : 'Revenus',
-        data: this.state.figurePerMonth.map(el => el.revenues)
+        data: this.state.figurePerMonth.map(el => el.revenues.toFixed(0)),
+        backgroundColor : 
+          ['rgba(230, 61, 104, 0.5)',
+          'rgba(243, 180, 56, 0.5)',
+          'rgba(250, 220, 51, 0.5)',
+          'rgba(181, 250, 51 , 0.5)',
+          'rgba(51, 250, 220 , 0.5)',
+          'rgba(51, 178, 250, 0.5)',
+          'rgba(51, 69, 250, 0.5)',
+          'rgba(190, 51, 250 , 0.5)',
+          'rgba(212, 158, 118 , 0.5)',
+          'rgba(243, 180, 56, 0.5)',
+          'rgba(181, 250, 51 , 0.5)',
+          'rgba(51, 178, 250, 0.5)',
+          ],
+        borderWidth : 2,
+        hoverBackgroundColor : 'rgb(24, 21, 17,0.2)',
+        barThickness: 30,
       }]
     })
   }
-
 
   render(){
     return(
@@ -41,7 +58,20 @@ class ByMonthGlobal extends React.Component {
         <h2>Revenus globaux par mois</h2>
           <Bar
           data = {this.getChartDatas}
-          options = {{}}
+          height={300}
+          options={{ 
+            maintainAspectRatio: false,
+            responsive : true,
+            scales : {
+              yAxes : [{
+                ticks : {
+                  callback : function(value,index,values){
+                    return value + ' €' ;
+                  }
+                }
+              }]
+            }
+          }}
           />
       </div>
     )
